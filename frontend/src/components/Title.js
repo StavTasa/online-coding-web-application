efcb44662253814195eb57bc33fc1bc9da15de4c
stavtasa <stavtasa@gmail.com>
2023-07-17 15:20:20 +0300

    add main title

---
+++ frontend/src/components/Title.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Title = () => {
+  return (
+    <Card className="text-center" border="light">
+      <Card.Body>
+        <Card.Title as="h3">Choose code block</Card.Title>
+        <Card.Text>
+ 
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Title;
